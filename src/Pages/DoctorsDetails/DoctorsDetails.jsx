3d44a51b1567e1c29aef9e1c9cb9dbeda54d3a01
shakilahmad777 <shakilahmadpip3@gmail.com>
2026-03-13 20:00:46 +0600

    color

--- src/Pages/DoctorsDetails/DoctorsDetails.jsx
+++ src/Pages/DoctorsDetails/DoctorsDetails.jsx
@@ -21,7 +21,7 @@ export default function DoctorsDetails() {
 
     return (
         <div className=''>
-            <div className="bg-[#FFFFFF] rounded-[1rem] mb-5 py-15 text-center shadow-(--header-shadow)">
+            <div className="!bg-[#FFFFFF] rounded-[1rem] mb-5 py-15 text-center shadow-(--header-shadow)">
                 <h1 className='text-2xl md:text-5xl mb-5 shadow-(--header-shadow) p-3'>Doctors Profile Details</h1>
                 <div className="flex justify-center">
                     <div className="md:w-[70%] ">
@@ -31,27 +31,27 @@ export default function DoctorsDetails() {
             </div>
             {/* doctores profile details end */}
 
-            <div className="md:flex gap-5 bg-[#FFFFFF] rounded-[1rem] p-5 md:p-15 shadow-(--header-shadow)">
+            <div className="md:flex gap-5 !bg-[#FFFFFF] rounded-[1rem] p-5 md:p-15 shadow-(--header-shadow)">
                 <div className="flex justify-center">
-                    <img src={image} className='shadow rounded-[1rem] w-96 h-64 bg-[#FFFFFF]' alt="" />
+                    <img src={image} className='shadow rounded-[1rem] w-96 h-64 !bg-[#FFFFFF]' alt="" />
                 </div>
                 <div className="flex flex-col leading-7">
                     <h1>{name}</h1>
                     <small>{education}</small>
                     <small>Working at</small>
                     <p>{hospital}</p>
-                    <small className='border-y-1 border-dashed my-3 border-orange-300'>{registrationNumber}</small>
+                    <small className='border-y-1 border-dashed my-3 !border-orange-300'>{registrationNumber}</small>
                     <span className='flex flex-wrap gap-3 items-center '>
                         <small>Availability</small>
                         {
-                            on ? (schedule.workingDays.map((day, index) => <button className='btn border border-green-200 bg-green-50 text-green-500 rounded-full' key={index}>{day}</button>)) : (<button className='btn border border-orange-200 bg-orange-50 text-orange-500 rounded-full'>Not Available | Please Come <ArrowRight className='animate-ping'></ArrowRight></button>)
+                            on ? (schedule.workingDays.map((day, index) => <button className='btn border !border-green-200 !bg-green-50 !text-green-500 rounded-full' key={index}>{day}</button>)) : (<button className='btn border !border-orange-200 !bg-orange-50 !text-orange-500 rounded-full'>Not Available | Please Come <ArrowRight className='animate-ping'></ArrowRight></button>)
                         }
                         {
-                            on ? '' : (schedule.workingDays.map((day, index) => <small className='  text-green-500' key={index}>{day}</small>))
+                            on ? '' : (schedule.workingDays.map((day, index) => <small className='  !text-green-500' key={index}>{day}</small>))
                         }
                     </span>
                     <small>Consultation Fee: {consultationFee} Taka (include Vat) Per Consultation</small>
-                    <button className={`btn ${on ? 'bg-blue-600 text-white mt-3' : 'bg-red-400'}`} onClick={() => setOn(!on)}>
+                    <button className={`btn ${on ? '!bg-blue-600 text-white mt-3' : '!bg-red-400'}`} onClick={() => setOn(!on)}>
                         {
                             on ? 'ON' : 'OFF'
                         }
@@ -60,22 +60,22 @@ export default function DoctorsDetails() {
             </div>
             {/* doctors details end */}
 
-            <div className="bg-[#FFFFFF] rounded-[1rem] mt-5 py-15 shadow-(--header-shadow)">
+            <div className="!bg-[#FFFFFF] rounded-[1rem] mt-5 py-15 shadow-(--header-shadow)">
                 <div className="">
                     <h1 className='text-2xl md:text-3xl text-center shadow-(--header-shadow) p-3 mb-5'>Book an Appointment</h1>
                     <div className="flex justify-between mx-5">
                         <p>Availability</p>
                         {
-                            on ? (<button className='btn border border-green-200 bg-green-50 text-green-500 rounded-full'>Doctor Available Today</button>) : (<button className='btn border border-orange-200 bg-orange-50 text-orange-500 rounded-full'>Doctor Not Available Today</button>)
+                            on ? (<button className='btn border !border-green-200 !bg-green-50 !text-green-500 rounded-full'>Doctor Available Today</button>) : (<button className='btn border border-orange-200 bg-orange-50 text-orange-500 rounded-full'>Doctor Not Available Today</button>)
                         }
                     </div>
                 </div>
                 <div className="grid">
-                    <p className='md:flex gap-2 m-5 p-2 bg-orange-50 text-orange-400 rounded md:rounded-full'>
+                    <p className='md:flex gap-2 m-5 p-2 !bg-orange-50 !text-orange-400 rounded md:rounded-full'>
                         <BadgeAlert></BadgeAlert> Due to high patient volume, we currently accepting appointments for doday only. We appreciate your understanding and cooperation.
                     </p>
                     {
-                        on ? (<button onClick={() => {handleDoctorList(id), navigate('/bookings')}} className='btn bg-blue-500 rounded-full mx-10 text-white md:text-2xl py-5'>Book Appointment Now</button>) : (<button onClick={() => toast.error("Doctor Not Available")} className='btn bg-blue-500 rounded-full mx-10 text-white md:text-2xl py-5 opacity-50'>Book Appointment Now</button>)
+                        on ? (<button onClick={() => {handleDoctorList(id), navigate('/bookings')}} className='btn !bg-blue-500 rounded-full mx-10 !text-white md:text-2xl py-5'>Book Appointment Now</button>) : (<button onClick={() => toast.error("Doctor Not Available")} className='btn !bg-blue-500 rounded-full mx-10 !text-white md:text-2xl py-5 opacity-50'>Book Appointment Now</button>)
                     }
                 </div>
             </div>
--- src/component/Doctors/Doctors.jsx
+++ src/component/Doctors/Doctors.jsx
@@ -25,8 +25,8 @@ export default function Doctors() {
         <div className="">
             <div className="flex justify-center my-5">
                 <div className="w-[80%]">
-                    <h1 className='text-2xl md:text-5xl text-center md:leading-[3.5rem]'>Our Best Doctors</h1>
-                    <p className="!text-gary-700 text-center my-5">Our platform connects you with verified. exprenced doctors across vairous specialties -- all your convenience. Whether it's a routine checkup or urgent consultation book appointment in minutes and receive quality care you can trust</p>
+                    <h1 className='text-2xl md:text-5xl text-center md:leading-[3.5rem] !text-black'>Our Best Doctors</h1>
+                    <p className="!text-gray-500 text-center my-5">Our platform connects you with verified. exprenced doctors across vairous specialties -- all your convenience. Whether it's a routine checkup or urgent consultation book appointment in minutes and receive quality care you can trust</p>
                 </div>
             </div>
             <div className='grid md:grid-cols-2 lg:grid-cols-3 justify-center gap-5'>
