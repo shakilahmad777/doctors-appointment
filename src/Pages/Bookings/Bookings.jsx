0a6281dd44e7f5d572c1a2ac0608dc7d7567cc1f
shakilahmad777 <shakilahmadpip3@gmail.com>
2026-03-14 02:53:01 +0600

    thisssss

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -29,8 +29,8 @@ export default function Bookings() {
             </div>
             <div className="flex justify-center my-5">
                 <div className="w-[80%]">
-                    <h1 className='text-2xl md:text-5xl text-center md:leading-[3.5rem]'>My Appointments</h1>
-                    <p className="text-[#818181] text-center my-5">Our platform connects you with verified. exprenced doctors across vairous specialties -- all your convenience.</p>
+                    <h1 className='text-2xl md:text-5xl text-center !text-black md:leading-[3.5rem]'>My Appointments</h1>
+                    <p className="!text-gray-500 text-center my-5">Our platform connects you with verified. exprenced doctors across vairous specialties -- all your convenience.</p>
                 </div>
             </div>
             <div className="flex gap-5 flex-col">
